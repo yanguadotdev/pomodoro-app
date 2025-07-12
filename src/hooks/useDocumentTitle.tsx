1e32e0e3f5438e76f7c1fa0fb0b7e198d1142c3a
yanguadotdev <yanguasamir05@gmail.com>
2025-07-12 16:03:27 -0500

    add SEO meta tags and social sharing metadata to index.html

--- src/hooks/useDocumentTitle.tsx
+++ src/hooks/useDocumentTitle.tsx
@@ -6,6 +6,8 @@ interface DocumentTitleProps {
     formatTime: (seconds: number) => string
 }
 
+const DEFAULT_TITLE = 'Focus Pomodoro - Mejora tu concentración con sonidos naturales'
+
 export function useDocumentTitle({ timer, formatTime }: DocumentTitleProps) {
     useEffect(() => {
         if (timer.isRunning) {
@@ -13,11 +15,11 @@ export function useDocumentTitle({ timer, formatTime }: DocumentTitleProps) {
             const mode = timer.isBreak ? 'Descanso' : 'Estudio'
             document.title = `${timeStr} - ${mode} | Pomodoro Timer`
         } else {
-            document.title = 'Pomodoro Timer'
+            document.title = DEFAULT_TITLE
         }
 
         return () => {
-            document.title = 'Pomodoro Timer'
+            document.title = DEFAULT_TITLE
         }
     }, [timer.currentSeconds, timer.isBreak, timer.isRunning])
 }
